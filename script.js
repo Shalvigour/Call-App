516a02de3850d6baf3ba02a284c9e79a0166a372
Shalvi gour <shalvigour@gmail.com>
2026-05-19 03:03:11 +0530

    app ready

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let forminputs = document.querySelectorAll("form .inp")
 let submit = document.querySelector("form .btns-box input")
 let form_add = document.querySelector(".form-add")
 let close = document.querySelector("form .btns-box div")
+let add = document.querySelector(".btn-add .add-form")
+let up = document.querySelector(".btn-add .up")
+let down = document.querySelector(".btn-add .down")
 
-console.dir(forminputs[0])
 form.addEventListener("submit", function(event){
     let valid = true;
     for (let inp of forminputs){
@@ -25,6 +27,9 @@ form.addEventListener("submit", function(event){
     });
     form.style.display="none"
     form_add.style.display="flex"
+    event.preventDefault();
+    form.reset();
+    showCards();
 
 });
 
@@ -43,4 +48,104 @@ function saveToLocalStorage(obj){
 
 close.addEventListener("click",(event)=>{
     form.style.display="none";
+    form.reset();
+    form_add.style.display="flex";
+    showCards();
+
 })
+
+add.addEventListener("click",function(event){
+    form.style.display="flex";
+    form_add.style.display="none";
+
+})
+
+let stack = document.querySelector(".stack-main");
+let tasks = JSON.parse(localStorage.getItem("tasks"));
+console.log(tasks);
+function showCards(){
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    if(tasks!==null){
+        tasks.forEach((task)=>{
+            let frame1 = document.createElement("div");
+            frame1.classList.add("stack");
+
+            let img = document.createElement("div");
+            img.classList.add("image");
+            img.style.backgroundImage=`url(${task.imageUrl})`;
+            frame1.appendChild(img);
+
+            let h3 = document.createElement("h3");
+            h3.classList.add("card-name");
+            h3.innerText = task.name;
+            frame1.appendChild(h3);
+
+            let div1 = document.createElement("div");
+            div1.classList.add("card-city");
+            
+
+            let p1 = document.createElement("p");
+            p1.innerText="City";
+
+            let p2 = document.createElement("p");
+            p2.innerText=task.city;
+
+            div1.appendChild(p1);
+            div1.appendChild(p2);
+            frame1.appendChild(div1);
+
+            let div2 = document.createElement("div");
+            div2.classList.add("card-book");
+
+            let p3 = document.createElement("p");
+            p3.innerText="Purpose";
+
+            let p4 = document.createElement("p");
+            p4.innerText=task.purpose;
+
+            div2.appendChild(p3);
+            div2.appendChild(p4);
+
+            frame1.appendChild(div2);
+            let div3 = document.createElement("div");
+            div3.classList.add("card-btn");
+
+            let button1 = document.createElement("button");
+            button1.innerText="Call";
+
+            let button2 = document.createElement("button");
+            button2.innerText="Message";
+
+            div3.appendChild(button1);
+            div3.appendChild(button2);
+            frame1.appendChild(div3);
+
+            stack.appendChild(frame1);
+
+        })
+    }
+}
+
+up.addEventListener("click",(event)=>{
+    upCard();
+})
+
+down.addEventListener("click",(event)=>{
+    downCard();
+})
+
+function upCard(){
+    let obj = stack.firstElementChild;
+    if(obj){
+        stack.appendChild(obj);
+    }
+}
+
+function downCard(){
+    let obj = stack.lastElementChild;
+    if(obj){
+        stack.prepend(obj);
+    }
+}
+
+
